0c5ef983b467d8cd84fd5c5ea97e52b797677a2b
Patrick Slattery <slattery.patrickj@gmail.com>
2013-09-17 12:18:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitdemo::Application.config.secret_token = '8cfd236f4de9aa3a2d6389ffe92a57609ec65c40784b04f301fbbbd389c894b9d2453ecfd07e6c075f88764f52b2bfb9aa7a605ae96a0c69fa990403bd7e8d40'
